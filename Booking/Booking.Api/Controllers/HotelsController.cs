d1a5d0745c19f143d51fdb0d523ac6d19008f7c9
Turing99 <andrei.fratila99@yahoo.ro>
2022-06-10 19:59:30 +0300

    Added project structure, application layers and first controller

---
+++ Booking/Booking.Api/Controllers/HotelsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HotelsController : Controller
+    {
+        public HotelsController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetRooms()
+        { return Ok("Hello"); }
+    }
+}
---
+++ Booking/Booking.Domain/Models/Room.cs
@@ -0,0 +1,10 @@
+﻿namespace Booking.Domain.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public int RooNumber { get; set; }
+        public double Surface { get; set; }
+        public bool NeedsRepair { get; set; }
+    }
+}
